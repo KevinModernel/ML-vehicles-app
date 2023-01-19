9c8a56aac05df7b0566f913bcf86b90ed0c21cec
KevinModernel <modernelkevin96@gmail.com>
2023-01-19 01:38:39 -0300

    Remove unnecessary code and send JSON to front

--- src/controllers/index-controllers.js
+++ src/controllers/index-controllers.js
@@ -1,28 +1,15 @@
-var fs = require('fs');
 const { getRequestML } = require('../utils/ML-API-Utils.js');
 const { dataInResponse } = require('../utils/datahandler.js');
 const { getUSDToday } = require('../utils/USD-API-Utils.js');
 
-const displayForm = (req, res) => {
-	res.send("Hello World!");
-	//res.render('index');
-};
-
-const getData = async (req, res) => { // req.body.marca || "";
-	const marca = req.body.marca;
-	const modelo = req.body.modelo; 
+const getData = async (req, res) => {
+	const marca = req.params.marca;
+	const modelo = req.params.modelo; 
 	let storedData = await getRequestML(marca, modelo);
 	const cotizacionUSD = await getUSDToday();
 	storedData = dataInResponse(storedData, cotizacionUSD);
-	// console.log(JSON.stringify(storedData));
-	// console.log("hi");
-	var file = await fs.createWriteStream('array.JSON');
-	console.log("hi");
-	file.on('error', function(err) { console.log("error") });
-	file.write(JSON.stringify(storedData));
-	file.end();
-
-	res.render('index', { storedData } );
+	console.log("storedData: " + storedData);
+	res.json(storedData);
 };
 
-module.exports = { displayForm, getData }
+module.exports = { displayForm }
